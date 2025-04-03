ec3148e3133bcef5a4c81c996902be173c295245
Kristen George <kmstrumpfer@yahoo.com>
2025-04-02 20:57:33 -0400

    Finished

---
+++ index.js
@@ -0,0 +1,19 @@
+// UNIT TESTS
+// 1. happy - Expect code to return the product of two numbers
+//          - Expect the imput and product to be numbers
+//  unhappy - If a string is entered, display an error that says "Please enter two numbers"
+//            If less than 2 numbers are entered, display an error that says "Please enter two numbers"
+
+// 2. happy - Expect the function to only return odd numbers
+//          - Expect the numbers to be in acending order
+//          - Expect the numbers to not repeat
+//  unhappy - If there is a string in the array, display error
+//          - If there are no odd numbers in the array, it should push an empty array
+
+// FUNCTIONAL TESTS
+// 1. happy - Code should show a pop-up at check out that promps user to sign in or continue as a guest
+//          - The code should show the total of the cart
+//          - The code should allow entry of card details and address
+//          - The code should be able to save details if user has logged in before
+//  unhappy - If there is nothing in the cart, the code should display an error message
+//          - Display error if user tries to check out without entering required information
